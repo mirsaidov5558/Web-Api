eb9ed0b6b2f1ba03d32282851af37331b44203b9
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-02-12 14:18:41 +0500

    Class Reviews

---
+++ WebApi/Models/Reviews.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi.Models
+{
+    public class Reviews
+    {
+        public int ReviewsId {  get; set; }
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+        public string Rating { get; set; }
+        public string Comment { get; set; }
+    }
+}
