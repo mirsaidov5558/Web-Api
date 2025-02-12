16aa9769c16e9000bd94196b3e88aecc7ee56d14
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-02-12 14:17:37 +0500

    Class Products

---
+++ WebApi/Models/Products.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi.Models
+{
+    public class Products
+    {
+        public int ProductId { get; set; }
+        public int CategotyId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
