a12763b7859cba9596f0ae64294a33d35bbbcba8
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-02-12 14:15:43 +0500

    Class Category

---
+++ WebApi/Models/Categories.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.Models
+{
+    public class Categories
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string ParentCategoryId { get; set; }
+    }
+}
