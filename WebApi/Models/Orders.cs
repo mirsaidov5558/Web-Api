864ca2e66c1f97f2e6a01634c44714796fc90767
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-02-12 14:16:35 +0500

    Class Orders

--- WebApi/Models/Orders.cs
+++ WebApi/Models/Orders.cs
@@ -1,6 +1,13 @@
 ﻿namespace WebApi.Models
 {
+    
+
     public class Orders
     {
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreateAt { get; set; }
+        public string Status { get; set; }
+
     }
 }
