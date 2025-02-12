2c2600f3c195f3a3913d3ad7872796fec32c1c6e
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-02-12 12:54:57 +0500

    Initial commit of ASP.NET Core project

---
+++ WebApi/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            return Ok("Users");
+        }
+    }
+}
