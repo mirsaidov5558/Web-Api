d5c39158fb504a9b743e2c394b04ec91cd9caba4
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-02-12 14:17:09 +0500

    Class OrderItems

---
+++ WebApi/Models/OrderItems.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi.Models
+{
+    public class OrderItems
+    {
+        public int OrderItemId { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
