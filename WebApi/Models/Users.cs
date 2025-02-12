baca07b03cc8cc1e5fc2cefd2654e35a49baca1a
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-02-12 14:19:08 +0500

    Class Users

---
+++ WebApi/Models/Users.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi.Models
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash {  get; set; }
+        public string Role { get; set; }
+    }
+}
