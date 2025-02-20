e13b87609d2f2bf6665c20903a8b772e230be6b0
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-02-20 15:48:46 +0500

    OrderItemConfiguration

--- WebApi/Configuration/OrderItemConfiguration.cs
+++ WebApi/Configuration/OrderItemConfiguration.cs
@@ -1,6 +1,27 @@
-﻿namespace WebApi.Configuration
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApi.Models;
+
+namespace WebApi.Configuration
 {
-    public class OrderItemConfiguration
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
     {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.Quantity).HasMaxLength(50);
+            builder.Property(x => x.TotalPrice).HasColumnType("decimal(18,2)").IsRequired();
+
+            builder.HasOne(x => x.Order)
+                .WithMany(x => x.OrderItems)
+                .HasForeignKey(x => x.OrderId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(x => x.Product)
+                .WithMany(x => x.OrderItems)
+                .HasForeignKey(x => x.ProductId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
