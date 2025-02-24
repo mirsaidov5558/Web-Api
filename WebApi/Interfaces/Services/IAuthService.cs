5d55d60a5f3c43db1a99e7c3032b254bc10c07fa
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-02-24 21:25:07 +0500

    Interfaces

---
+++ WebApi/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using WebApi.Enums;
+
+namespace WebApi.Interfaces.Services
+{
+    public interface IAuthService
+    {
+        Task<string?> AuthenticateAsync(string email, string password);
+        Task<bool> RegisterAsync(string email, string password, Role role, string? firstName, string? lastName);
+    }
+}
