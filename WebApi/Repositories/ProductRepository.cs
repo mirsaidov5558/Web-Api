77f6ccafd5afe27e4f57b866ca954eb69b3173cb
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-02-24 23:05:52 +0500

    Repositories

---
+++ WebApi/Repositories/ProductRepository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+using WebApi.Models;
+using WebApi.Repositories.Interface;
+
+namespace WebApi.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async void Delete(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<List<Product>> GetAllAsync()
+        {
+            return await _context.Products.Include(p => p.Category).ToListAsync();
+        }
+
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            return await _context.Products.Include(p => p.Category).FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<List<Product>> GetFilteredProductsAsync(int? categoryId, string? productName, decimal? minPrice, decimal? maxPrice)
+        {
+            var query = _context.Products.AsQueryable();
+            if (categoryId.HasValue)
+                query = query.Where(p => p.CategoryId == categoryId.Value);
+
+            if (!string.IsNullOrEmpty(productName))
+                query = query.Where(p => p.ProductName.Contains(productName));
+
+            if (minPrice.HasValue)
+                query = query.Where(p => p.Price >= minPrice.Value);
+
+            if (maxPrice.HasValue)
+                query = query.Where(p => p.Price <= maxPrice.Value);
+
+            return await query.Include(p => p.Category).ToListAsync();
+        }
+
+        public async void Update(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();  
+        }
+    }
+}
