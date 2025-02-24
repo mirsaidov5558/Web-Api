3bc05ff00a710956e012b3f17c6c3cedc6727344
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-02-24 22:39:39 +0500

    Context

--- WebApi/Context/ApplicationDbContext.cs
+++ WebApi/Context/ApplicationDbContext.cs
@@ -10,7 +10,7 @@ namespace WebApi.Data
             
         }
 
-        public DbSet<List<Category>> Categories {  get; set; }
+        public DbSet<Category> Categories {  get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
