d716b83932f6b694e8df46927c0b9e03ad65ae60
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-02-28 18:06:55 +0500

    Services

---
+++ WebApi/Services/AuthService.cs
@@ -0,0 +1,19 @@
+﻿using WebApi.Enums;
+using WebApi.Interfaces.Services;
+
+namespace WebApi.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IAuthService _authService;
+        public Task<string?> AuthenticateAsync(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RegisterAsync(string email, string password, Role role, string? firstName, string? lastName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
