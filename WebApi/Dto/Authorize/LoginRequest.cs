bd32b7ead86ae4a2ec666245b98ebfe26b8e760a
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-02-28 17:50:31 +0500

    Dto

---
+++ WebApi/Dto/Authorize/LoginRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Dto.Authorize
+{
+    public class LoginRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Неправильный пароль")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ WebApi/Dto/Authorize/RegistRequest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Dto.Authorize
+{
+    public class RegistRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string? FirstName { get; set; } = string.Empty;
+        public string? LastName { get; set;} = string.Empty;
+
+        [Required]
+        [MinLength(6, ErrorMessage = "Пароль должен содержать минимум 6 символов.")]
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
