2aeb76579522adb9e16f876c0ea047231a3fcd38
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-02-28 18:04:30 +0500

    Responses

---
+++ WebApi/Responses/ApiResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApi.Responses
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; } // Статус запроса 
+        public string Message { get; set; } // Сообщение (ошибка, успех)
+        public T Data { get; set; } // Данные ответа
+                                    
+        public ApiResponse(T data, string message = "") // Конструктор для успешного ответа
+        {
+            Success = true;
+            Message = message;
+            Data = default;
+        }
+
+        public ApiResponse(string errorMessage) // Конструктор для ответа с ошибкой
+        {
+            Success = false;
+            Message = errorMessage;
+            Data = default;
+        }
+    }
+}
