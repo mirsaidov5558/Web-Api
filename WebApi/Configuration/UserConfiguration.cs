2bc12aec290c9ed7730b30c931a839793f7ac2fe
mirsaidov1 <mansurmirsaidov5558@gmail.com>
2025-03-01 15:31:33 +0500

    Configuration

--- WebApi/Configuration/UserConfiguration.cs
+++ WebApi/Configuration/UserConfiguration.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Org.BouncyCastle.Crypto.Generators;
+using WebApi.Enums;
 using WebApi.Models;
 
 namespace WebApi.Configuration
@@ -11,7 +13,8 @@ namespace WebApi.Configuration
         {
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).ValueGeneratedOnAdd();
-            builder.Property(x => x.UserName).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.FirstName).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.LastName).IsRequired().HasMaxLength(100);
             builder.Property(x => x.Email).IsRequired().HasMaxLength(255);
             builder.Property(x => x.PasswordHash).IsRequired();
             
@@ -25,8 +28,17 @@ namespace WebApi.Configuration
                 .WithOne(x => x.User)
                 .HasForeignKey(x => x.UserId)
                 .OnDelete(DeleteBehavior.Cascade);
-            
 
+            // Добавляем админа
+            builder.HasData(new User
+            {
+                Id = 1,
+                FirstName = "Admin",
+                LastName = "Admin",
+                Email = "admin@shop.com",
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword("Admin123"),
+                Role = Role.Admin
+            });
         }
     }
 
